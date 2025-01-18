0f650a7fb6de268cccc53d743504a7468d6c994c
kitoyaka <vlasiukdima007@gmail.com>
2025-01-18 12:35:29 +0200

    Added SFML library

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
-#include <iostream>
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+int main()
+{
+    // Create the main window
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "SFML window");
+
+    // Start the game loop
+    while (window.isOpen())
+    {
+        sf::Event event;
+        // Process events
+        while (window.pollEvent(event))
+        {
+            // Close window: exit
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        // Clear screen
+        window.clear();
+        // Update the window
+        window.display();
+    }
 }
