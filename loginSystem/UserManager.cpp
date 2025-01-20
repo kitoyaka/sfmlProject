1b4422a67217d5cddc8336320ba2bb555cf8beb8
kitoyaka <vlasiukdima007@gmail.com>
2025-01-20 12:34:37 +0200

    Added loadUsers and saveUsers functions

--- loginSystem/UserManager.cpp
+++ loginSystem/UserManager.cpp
@@ -3,3 +3,37 @@
 //
 
 #include "UserManager.h"
+
+void UserManager::loadUsers() {
+    std::ifstream  file(filename);
+    if(!file.is_open())
+    {
+        std::cerr << "Could not open the file: " << filename << std::endl;
+    }
+    std::string line;
+    while(std::getline(file, line))
+    {
+        std::ifstream  iss(line);
+        std::string login, password;
+        if(std::getline(iss, login, ':') && std::getline(iss, password))
+        {
+            users[login] = password;
+        }
+    }
+
+    file.close();
+}
+
+void UserManager::saveUsers() {
+    std::ofstream file(filename);
+    if(file.is_open())
+    {
+        std::cerr << "Could not oper the file: " << filename << std:: endl;
+    }
+    for(auto& [login, password] : users)
+    {
+        file << login << ":" << password << "\n";
+    }
+
+    file.close();
+}
--- loginSystem/UserManager.h
+++ loginSystem/UserManager.h
@@ -8,6 +8,7 @@
 #include <string>
 #include <fstream>
 #include <sstream>
+#include <iostream>
 
 class UserManager {
 private:
